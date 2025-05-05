65ebdb848471bd4ec63f2116ea3df63c960ab731
junyeong <557987@naver.com>
2025-05-05 23:57:51 +0900

    processor list insert added

--- processor.c
+++ processor.c
@@ -4,6 +4,41 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int process_counter = 0;
+
+/*
+ * create processor
+ */
+struct Processor *create_processor() {
+    struct Processor *processor = malloc(sizeof(struct Processor));
+    if (processor == NULL) {
+        return NULL;
+    }
+    processor->processor_id = ++process_counter;
+    processor->task_list = NULL;
+    processor->task_count = 0;
+    processor->next = NULL;
+    return processor;
+}
+
+/*
+ * create processor and insert into list
+ */
+int insert_processor_to_list(struct Processor **processor_list) {
+    struct Processor *new_processor = create_processor();
+    if (new_processor == NULL) {
+        return -1;
+    }
+    if(*processor_list == NULL) {
+        *processor_list = new_processor;
+    }else {
+        new_processor->next = (*processor_list)->next;
+        (*processor_list)->next = new_processor;
+    }
+    return 0;
+}
+
+
 /*
  *  insert task into processor's task_list
  */
@@ -13,6 +48,7 @@ int insert_task_to_processor(struct Processor *proc, struct Task *task_list) {
     }
     struct Task **target_list = &proc->task_list;
     insert_task(target_list);
+    proc->task_count++;
     return 0;
 }
 
@@ -26,6 +62,7 @@ int consume_task_from_processor(struct Processor *proc) {
     if(remove_task == NULL) {
         return -1;
     }
+    proc->task_count--;
     sleep(remove_task->running_time);
     free(remove_task);
     return 0;
--- processor.h
+++ processor.h
@@ -2,11 +2,20 @@
 #define PROCESSOR_H
 
 #include "task.h"
+#define for_each(pos, head) for(pos = (head)->next; pos != (head); pos = pos->next)
+
+
+extern int processor_counter;
 
 struct Processor {
     int processor_id;
     int task_count;
     struct Task *task_list;
+    struct Processor *next;
 };
 
+int insert_task_to_processor(struct Processor *proc, struct Task *task_list);
+int consume_task_from_processor(struct Processor *proc);
+struct Processor *create_processor();
+int insert_processor_to_list(struct Processor **processor_list);
 #endif
